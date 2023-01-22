26aa5196e5b9eefa1065504372295fa30e52a734
Dylan Cerenov <dcerenov25@gmail.com>
2023-01-22 16:43:06 -0500

    Commiting final froggy
    Co-authored-by: SuliR123 <SuliR123@users.noreply.github.com>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <div class="flex w-screen h-screen">
+    <img class="w-80 h-50" src="https://cdn.dribbble.com/users/1044993/screenshots/5945543/media/4f75b2ce7a66bb8f3704f7fcbb40d8f9.png?compress=1&resize=400x300&vertical=top"></img>
     <App />
-  </React.StrictMode>
-);
+  </div> 
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
